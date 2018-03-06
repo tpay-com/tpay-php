6814214ea7a981d4600cdbbb499b05ef3d816cfd
Piotr Jóźwiak <piotr.jozwiak@tpay.com>
2018-03-06 12:33:35 +0100

    New credit card payment example

--- tpayLibs/examples/CardGate.php
+++ tpayLibs/examples/CardGate.php
@@ -7,6 +7,8 @@
 namespace tpayLibs\examples;
 
 use tpayLibs\src\_class_tpay\PaymentForms\PaymentCardForms;
+use tpayLibs\src\_class_tpay\Utilities\Util;
+use tpayLibs\src\Dictionaries\FieldsConfigDictionary;
 
 include_once 'config.php';
 include_once 'loader.php';
@@ -24,32 +26,61 @@ class CardGate extends PaymentCardForms
         parent::__construct();
     }
 
-    public function getCardDirectHtml()
+    public function init()
     {
-        $cardGateHTML = $this->getOnSiteCardForm('SecureSalePayment.php');
-
-        $exampleCardData = array(
-            'number' => '4532823576358083',
-            'csc' => '976',
-            'exp' => '03 / 18',
-            'name' => 'John Doe',
-            'email' => 'customer@example.com',
-        );
-
-        echo '<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Card payment</title>
-    <meta name="description" content="Card payment">
-</head>
-<body>
-' . $cardGateHTML . '
-<pre>' . print_r($exampleCardData, true) . '</pre>
-</body>
-</html>';
-
+        if (empty($_POST)) {
+            //Show new payment form
+            echo $this->getOnSiteCardForm('CardGate.php');
+        } else {
+            //Try to sale with provided card data
+            $response = $this->makeCardPayment();
+            //Successful payment by card not protected by 3DS
+            if (isset($response['result']) && (int)$response['result'] === 1) {
+                $this->setOrderAsComplete($response);
+                //Successfully generated 3DS link for payment authorization
+            } elseif (isset($response['3ds_url'])) {
+                header("Location: " . $response['3ds_url']);
+            } else {
+                //Invalid credit card data
+                $this->tryToSaleAgain();
+            }
+        }
     }
+
+    private function makeCardPayment($failOver = false)
+    {
+        $cardData = Util::post('carddata', FieldsConfigDictionary::STRING);
+        $clientName = Util::post('client_name', FieldsConfigDictionary::STRING);
+        $clientEmail = Util::post('client_email', FieldsConfigDictionary::STRING);
+        $saveCard = Util::post('card_save', FieldsConfigDictionary::STRING);
+        Util::log('Secure Sale post params', print_r($_POST, true));
+        if ($saveCard === 'on') {
+            $this->setOneTimer(false);
+        }
+        $this->setAmount(123)->setCurrency(985)->setOrderID('test payment 123');
+        $this->setLanguage('en')->setReturnUrls('https://tpay.com', 'https://google.pl');
+
+        return $failOver === false ?
+            $this->registerSale($clientName, $clientEmail, 'test sale', $cardData) :
+            $this->setCardData(null)->registerSale($clientName, $clientEmail, 'test sale');
+    }
+
+    private function setOrderAsComplete($params)
+    {
+        var_dump($params);
+    }
+
+    private function tryToSaleAgain()
+    {
+        //Try to create new transaction and redirect customer to Tpay transaction panel
+        $response = $this->makeCardPayment(true);
+        if (isset($response['sale_auth'])) {
+            header("Location: " . 'https://secure.tpay.com/cards/?sale_auth=' . $response['sale_auth']);
+        } else {
+            echo $response['err_desc'];
+        }
+    }
+
 }
 
-(new CardGate())->getCardDirectHtml();
+(new CardGate())->init();
