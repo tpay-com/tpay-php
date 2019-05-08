f56c81ba882b0ff88f931c2b12b8262abc0fe6b4
Piotr Jóźwiak <piotr.jozwiak@tpay.com>
2019-05-08 17:18:40 +0200

    Additional logs in Transaction API refunds

--- tpayLibs/src/_class_tpay/Refunds/BasicRefunds.php
+++ tpayLibs/src/_class_tpay/Refunds/BasicRefunds.php
@@ -1,56 +1,46 @@
 <?php
-
-/*
- * Created by tpay.com.
- * Date: 13.06.2017
- * Time: 15:50
- */
-
 namespace tpayLibs\src\_class_tpay\Refunds;
 
 use tpayLibs\src\_class_tpay\Utilities\TException;
 use tpayLibs\src\_class_tpay\TransactionApi;
+use tpayLibs\src\_class_tpay\Utilities\Util;
 
 class BasicRefunds extends TransactionApi
 {
     /**
-     * Refund all amount to customer
-     *
+     * Refund full amount to customer
      * @return array
-     *
      * @throws TException
      */
     public function refund()
     {
-        $url = $this->apiURL . $this->trApiKey . '/chargeback/transaction';
-
-        $response = $this->requests($url, array(static::TITLE => $this->transactionID));
-
+        $url = $this->apiURL.$this->trApiKey.'/chargeback/transaction';
+        $requestData = [static::TITLE => $this->transactionID];
+        Util::log('Refund request', print_r($requestData, true));
+        $response = $this->requests($url, $requestData);
         $this->checkError($response);
+        Util::log('Refund response', print_r($response, true));
 
         return $response;
     }
 
     /**
      * Refund custom amount to customer
-     *
      * @param float $amount refund amount
-     *
      * @return array
-     *
      * @throws TException
      */
     public function refundAny($amount)
     {
-        $url = $this->apiURL . $this->trApiKey . '/chargeback/any';
-
-        $postData = array(
-            static::TITLE       => $this->transactionID,
+        $url = $this->apiURL.$this->trApiKey.'/chargeback/any';
+        $requestData = [
+            static::TITLE => $this->transactionID,
             'chargeback_amount' => $amount,
-        );
-        $response = $this->requests($url, $postData);
-
+        ];
+        Util::log('Refund any request', print_r($requestData, true));
+        $response = $this->requests($url, $requestData);
         $this->checkError($response);
+        Util::log('Refund any response', print_r($response, true));
 
         return $response;
     }
