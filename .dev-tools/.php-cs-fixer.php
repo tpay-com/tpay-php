702470c3e64896c062209e5c406fde2d3e1d8534
Kuba Werłos <werlos@gmail.com>
2023-08-08 09:50:52 +0200

    Add "concat_space" rule

--- .dev-tools/.php-cs-fixer.php
+++ .dev-tools/.php-cs-fixer.php
@@ -4,12 +4,19 @@ return (new PhpCsFixer\Config())
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->ignoreDotFiles(false)
-            ->in(__DIR__ . '/..')
+            ->in(__DIR__.'/..')
     )
     ->setRules([
         '@PSR12' => true,
         'array_indentation' => true,
         'array_syntax' => true,
         'binary_operator_spaces' => true,
+        'cast_spaces' => ['space' => 'none'],
+        'class_definition' => [
+            'multi_line_extends_each_single_line' => true,
+            'single_item_single_line' => true,
+            'space_before_parenthesis' => true,
+        ],
+        'concat_space' => true,
         'visibility_required' => ['elements' => ['method']],
     ]);
