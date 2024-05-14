b8496cdf7497d42c63e71ac07789eecd3df81cdf
krzGablo <147171777+krzGablo@users.noreply.github.com>
2024-05-14 14:14:12 +0200

    Credentials fix in examples (#72)

--- examples/CardGateExtended.php
+++ examples/CardGateExtended.php
@@ -21,10 +21,10 @@ final class CardGateExtended extends PaymentCardForms
     {
         // This is pre-configured sandbox access. You should use your own data in production mode.
         $this->cardApiKey = 'bda5eda723bf1ae71a82e90a249803d3f852248d';
-        $this->cardApiPass = 'IhZVgraNcZoWPLgA';
-        $this->cardKeyRSA = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0NCk1JR2ZNQTBHQ1NxR1NJYjNEUUVCQVFVQUE0R05BRENCaVFLQmdRQ2NLRTVZNU1Wemd5a1Z5ODNMS1NTTFlEMEVrU2xadTRVZm1STS8NCmM5L0NtMENuVDM2ekU0L2dMRzBSYzQwODRHNmIzU3l5NVpvZ1kwQXFOVU5vUEptUUZGVyswdXJacU8yNFRCQkxCcU10TTVYSllDaVQNCmVpNkx3RUIyNnpPOFZocW9SK0tiRS92K1l1YlFhNGQ0cWtHU0IzeHBhSUJncllrT2o0aFJDOXk0WXdJREFRQUINCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ';
+        $this->cardApiPass = 'IhZVgraNcZoWPLgA1yQcGMIzquVWWrWtJ';
+        $this->cardKeyRSA = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0NCk1JR2ZNQTBHQ1NxR1NJYjNEUUVCQVFVQUE0R05BRENCaVFLQmdRQ2NLRTVZNU1Wemd5a1Z5ODNMS1NTTFlEMEVrU2xadTRVZm1STS8NCmM5L0NtMENuVDM2ekU0L2dMRzBSYzQwODRHNmIzU3l5NVpvZ1kwQXFOVU5vUEptUUZGVyswdXJacU8yNFRCQkxCcU10TTVYSllDaVQNCmVpNkx3RUIyNnpPOFZocW9SK0tiRS92K1l1YlFhNGQ0cWtHU0IzeHBhSUJncllrT2o0aFJDOXk0WXdJREFRQUINCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ==';
         $this->cardVerificationCode = '6680181602d396e640cb091ea5418171';
-        $this->cardHashAlg = 'sha1';
+        $this->cardHashAlg = 'sha512';
         parent::__construct();
     }
 
